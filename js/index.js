ef9cc350c5a4ce6a4bd77062ac412292caf71c78
vanyyaa11 <verkholiakivan@yahoo.com.ua>
2019-06-18 20:06:52 +0000

    mainproject

--- js/index.js
+++ js/index.js
@@ -1,50 +1,49 @@
-window.addEventListener('load', ()=> {
+window.addEventListener("load", () => {
     let long;
     let lat;
-    let temperatureDescription = document.querySelector(".temperature-degree");
-    let locationtimezone = document.querySelector(".location-timezone");
-    let temperatureSection = document.querySelector("temperature");
-    const temperatureSpan = document.querySelector('temperature span');
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position =>{
-            long = position.coords.longtitude;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
+    let temperatureSection = document.querySelector(".temperature");
+    const temperatureSpan = document.querySelector(".temperature span");
+    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
             lat = position.coords.latitude;
-           
-            const proxy = "https://cors-anywhere.herokuapp.com/;
-            const api = '${proxy}
-            https://api.darksky.net/forecast/fced929ddd4a16a922dc407e7b128c59/${long},${lat};
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            let api = `${proxy}https://api.darksky.net/forecast/fced929ddd4a16a922dc407e7b128c59/${lat},${long}`;
         
             fetch(api)
-              .then(response=> {
+              .then(response => {
                 return response.json(); 
                })
                 .then(data => {
-                    const {temperature, summary, icon} = data.curently;
+                    const {temperature, summary, icon} = data.currently;
                     //
-                    temperatureDegree.textContent = temperature;
-                    temperatureDescription.textContent = Summary;
-                    locationTimezone.textContent = data.timezone;
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
                      // formula for celsius
-                     let celsius = (temperature - 32)*(5/9);
-                     
-                     setIcons(icon, document.querySelector(".icon"));
+                let celsius = (temperature - 32)*(5/9);
+                setIcons(icon, document.querySelector(".icon"));
                      
                      // celsius to farenheit
-                     temperatureSection.addEventListener("click", () => {
-                         if (temperatureSpan.textContent === 'F') {
-                             temperatureSpan.textContent = "C";
-                             temperatureDegree.textContent = Math.floor(celsius);
+                temperatureSection.addEventListener("click", () => {
+                    if (temperatureSpan.textContent === 'F') {
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = Math.floor(celsius);
                          } else {
-                             temperatureSpan.textContent = "F";
-                             temperatureDegree.textContent = temperature;
+                            temperatureSpan.textContent = "F";
+                            temperatureDegree.textContent = temperature;
                          }
                      })
                 });
             });
     }
     function setIcons(icon, iconID) {
-        const skycons = new skycons({color: "white"});
-        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        const skycons = new Skycons({color: "white"});
+        let currentIcon = icon.replace(/-/g, "_").toUpperCase();
         skycons.play();
         return skycons.set(iconID, Skycons[currentIcon]);
     }
